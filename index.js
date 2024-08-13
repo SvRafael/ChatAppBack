5957276a13b5218c3baf4e2f4d9fbf5ed58a245d
SvRafael <rafaelsvsouza@hotmail.com>
2024-08-13 00:36:48 -0300

    feat: connect mongoose mongo atlas

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require("express")
 const cors = require("cors")
+const mongoose = require("mongoose")
 
-const app = express()
+const app = express();
+require("dotenv").config();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, (req, res) => {
     console.log("Server running!");
+})
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri).then(() => {
+    console.log("DB Connected...")
+}).catch((erros) => {
+    console.log("DB Connection failed: ", erros.message)
 })
 
